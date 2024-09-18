59c0f12f663c0ddb7612135610aabba0896f4b39
Mathx8 <matheusqueirozdev@gmail.com>
2024-09-18 18:14:07 -0300

    responsividade parte 2

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ function esconderAba() {
     if (aba.style.display === "flex" || aba.style.display === "") {
         aba.style.display = "none";
         toggleIcon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="#e8eaed">
-            <path d="m309.67-81.33-61-61.67L587-481.33 248.67-819.67l61-61.66 400 400-400 400Z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="44px" fill="#e8eaed">
+            <path d="M200-200v-240h80v160h160v80H200Zm480-320v-160H520v-80h240v240h-80Z"/>
         </svg>`;
     } else {
         aba.style.display = "flex";
         toggleIcon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="#e8eaed">
-            <path d="M400-80 0-480l400-400 61 61.67L122.67-480 461-141.67 400-80Z" />
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="44px" fill="#e8eaed">
+            <path d="M440-440v240h-80v-160H200v-80h240Zm160-320v160h160v80H520v-240h80Z"/>
         </svg>`;
     }
 }
