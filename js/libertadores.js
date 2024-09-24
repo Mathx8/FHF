e6f58fcebe197c8cecf5ef462253d9f219729cf9
Mathx8 <matheusqueirozdev@gmail.com>
2024-09-24 14:58:37 -0300

    automaticação da rolagem das rodadas

--- js/libertadores.js
+++ js/libertadores.js
@@ -9,20 +9,21 @@ function proximaRodada() {
     if (rodada1.style.display === "flex" || rodada1.style.display === "") {
         rodada1.style.display = "none";
         rodada2.style.display = "flex";
-    } else if(rodada2.style.display === "flex") {
+    } else if (rodada2.style.display === "flex") {
         rodada2.style.display = "none";
         rodada3.style.display = "flex";
-    } else if(rodada3.style.display === "flex") {
+    } else if (rodada3.style.display === "flex") {
         rodada3.style.display = "none";
         rodada4.style.display = "flex";
-    } else if(rodada4.style.display === "flex") {
+    } else if (rodada4.style.display === "flex") {
         rodada4.style.display = "none";
         rodada5.style.display = "flex";
-    } else if(rodada5.style.display === "flex") {
+    } else if (rodada5.style.display === "flex") {
         rodada5.style.display = "none";
         rodada6.style.display = "flex";
-    } else {
-        alert("Rodada não encontrada")
+    } else if (rodada6.style.display === "flex") {
+        rodada6.style.display = "none";
+        rodada1.style.display = "flex";
     }
 }
 
@@ -37,19 +38,25 @@ function voltarRodada() {
     if (rodada6.style.display === "flex" || rodada1.style.display === "") {
         rodada6.style.display = "none";
         rodada5.style.display = "flex";
-    } else if(rodada5.style.display === "flex") {
+    } else if (rodada5.style.display === "flex") {
         rodada5.style.display = "none";
         rodada4.style.display = "flex";
-    } else if(rodada4.style.display === "flex") {
+    } else if (rodada4.style.display === "flex") {
         rodada4.style.display = "none";
         rodada3.style.display = "flex";
-    } else if(rodada3.style.display === "flex") {
+    } else if (rodada3.style.display === "flex") {
         rodada3.style.display = "none";
         rodada2.style.display = "flex";
-    } else if(rodada2.style.display === "flex") {
+    } else if (rodada2.style.display === "flex") {
         rodada2.style.display = "none";
         rodada1.style.display = "flex";
-    } else {
-        alert("Rodada não encontrada")
     }
+}
+
+function iniciarAutoRotacao() {
+    setInterval(proximaRodada, 7000);
+}
+
+window.onload = function () {
+    iniciarAutoRotacao();
 }
